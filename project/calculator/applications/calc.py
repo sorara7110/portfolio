510eefced22ac7d6c925f23c60085031bffa500f
sorara7110 <191w00085@rissho-univ.jp>
2021-12-20 22:01:41 +0900

    update calculator

--- project/calculator/applications/calc.py
+++ project/calculator/applications/calc.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import math
 import io
 
 def diversity_calc(csv_path):
@@ -10,23 +9,31 @@ def diversity_calc(csv_path):
     # 多様度指数Hの計算
     # 正しい形式か確認
     try:
-        pop = df.iloc[:,1].sum()
-        H = 0
-        for i in range(len(df)):
-            Pi = df.iloc[i,1] / pop
-            H -= Pi * math.log2(Pi)
-        H = f"{H:.2f}"
+        data = df.iloc[:,1:]
+        pop = data.sum().values
+        Pi = data / pop
+        h = -Pi * np.log2(Pi)
+        H = h.sum()
     except TypeError:
-        H = "error"
-        graph = "none"
-        return H, graph
+        graph = "error"
+        return graph
 
     # グラフ作成
     plt.rcParams['font.family'] = "IPAexGothic" # 日本語対応のフォントに変更
     plt.style.use("ggplot")
     fig, ax = plt.subplots()
-    plt.barh(df.iloc[:,0] ,df.iloc[:,1])
-    plt.xlabel("個体数")
+    plt.barh(data.columns, H)
+    plt.xlabel("多様度指数H'")
     plt.savefig("/django/project/media/csv/graph.svg", bbox_inches='tight')
     graph = f"/media/csv/graph.svg"
-    return H, graph
+
+    # 表作成
+    H = [list(np.round(H, 2))]
+    columns = data.columns
+    fig, ax = plt.subplots()
+    plt.table(cellText=H, colLabels=columns, loc="center")
+    plt.axis("off")
+    plt.axis('tight')
+    plt.savefig("/django/project/media/csv/table.svg", bbox_inches='tight')
+    table = f"/media/csv/table.svg"
+    return graph, table
--- project/calculator/views.py
+++ project/calculator/views.py
@@ -8,32 +8,30 @@ def calculator(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
 
+        # リクエストがポストだった場合の処理
         # 過去のファイルの削除
         if form.is_valid():
+
+            # 過去のグラフが残っていれば削除
             if os.path.isfile("/django/project/media/graph.svg"):
                 os.remove("/django/project/media/graph.svg")
-            else:
-                pass
-            
+
+            # 過去の表が残っていれば削除
+            if os.path.isfile("/django/project/media/table.svg"):
+                os.remove("/django/project/media/table.svg")
+
+            # 過去のcsvファイルが残っていれば削除
             csv_data = form.save()
             pk = csv_data.pk - 1
             if Csv_read.objects.filter(pk=pk).exists():
                 Csv_read.objects.get(pk=pk).delete()
-            else:
-                pass
 
             # 多様度指数算出関数の呼び出し
-            H, graph = diversity_calc(csv_data.file)
+            graph, table = diversity_calc(csv_data.file)
 
-            # アップロードされたCSVファイルの形式が正しくなければトップページヘ
-            if H == "error":
-                form = UploadFileForm()
-                return render(request, 'calculator/home.html', {"form": form})
-            else:
-                return render(request, 'calculator/diversity_result.html', {"form": form, "H": H, "graph": graph})
-
-
-    else:
-        form = UploadFileForm()
-
+            # アップロードされたcsvファイルの形式が正しくなければトップページヘ
+            if graph != "error":
+                return render(request, 'calculator/diversity_result.html', {"form": form, "graph": graph, "table": table})
+    
+    form = UploadFileForm()
     return render(request, 'calculator/home.html', {"form": form})
